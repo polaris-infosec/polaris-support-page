b23e97ef0c725506a29b4c3a13fbf87df5121b17
donghieu <hieund@vzota.com.vn>
2023-08-24 17:24:04 +0700

    update homepage button link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/">
+            to="/docs/quick-start/introduction">
             <Translate>Hướng dẫn Polaris - 5 phút ⏱️</Translate>
           </Link>
         </div>
