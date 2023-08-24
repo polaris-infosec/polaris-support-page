a363b5bc3f3673713e2fd9d7d72de29eb4772c93
donghieu <hieund@vzota.com.vn>
2023-08-24 17:35:23 +0700

    update baseurl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   url: 'https://support.polarisec.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: process.env.BASE_URL || '/',
+  baseUrl: '/polaris-support-test-site/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
