e49c704dde14c14efd243a82df9fde1b629795ab
donghieu <hieund@vzota.com.vn>
2023-09-12 13:25:12 +0700

    update ip addresses

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'DongHieuA7', // Usually your GitHub org/user name.
+  organizationName: 'polaris-infosec', // Usually your GitHub org/user name.
   projectName: 'polaris-support', // Usually your repo name.
 
   onBrokenLinks: 'throw',
@@ -43,13 +43,7 @@ const config = {
           editUrl:
             'https://github.com/polaris-infosec/polaris-support/blob/master/',
         },
-        // blog: {
-        //   showReadingTime: true,
-        //   // Please change this to your repo.
-        //   // Remove this to remove the "edit this page" links.
-        //   editUrl:
-        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        // },
+        blog: false,
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
@@ -105,55 +99,7 @@ const config = {
       },
       footer: {
         style: 'dark',
-        links: [
-          // {
-          //   title: 'Docs',
-          //   items: [
-          //     {
-          //       label: 'Quick Start',
-          //       to: '/docs/quick-start',
-          //     },
-          //     {
-          //       label: 'Security Center',
-          //       to: '/docs/security-center',
-          //     },
-          //     {
-          //       label: 'General Knowledge',
-          //       to: '/docs/general-knowledge',
-          //     },
-          //   ],
-          // },
-          // {
-          //   title: 'Community',
-          //   items: [
-          //     {
-          //       label: 'Stack Overflow',
-          //       href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-          //     },
-          //     {
-          //       label: 'Discord',
-          //       href: 'https://discordapp.com/invite/docusaurus',
-          //     },
-          //     {
-          //       label: 'Twitter',
-          //       href: 'https://twitter.com/docusaurus',
-          //     },
-          //   ],
-          // },
-          // {
-          //   title: 'More',
-          //   items: [
-          //     {
-          //       label: 'Blog',
-          //       to: '/blog',
-          //     },
-          //     {
-          //       label: 'GitHub',
-          //       href: 'https://github.com/facebook/docusaurus',
-          //     },
-          //   ],
-          // },
-        ],
+        links: [],
         copyright: `Copyright © ${new Date().getFullYear()} Polaris Infosec`,
       },
       prism: {
